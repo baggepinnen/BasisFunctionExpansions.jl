dd54b11efc6cb0742bd7db528121c45a061661bf
Fredrik Bagge Carlson <cont-frb@ulund.org>
2017-02-08 18:46:05 +0100

    Now functional

--- src/BasisFunctionExpansions.jl
+++ src/BasisFunctionExpansions.jl
@@ -1,94 +1,101 @@
 module BasisFunctionExpansions
+using Base.Test
+export BasisFunctionExpansion, UniformRBFE, MultiUniformRBFE
 
-# From Robotlib  ===============================================================
-
-function basisParametersNd(p,centers, sigma, velocity::Int=0, normalize=true)
-    @assert size(p,2) == length(sigma)
-    N       = size(p,2)
-    N_basis = size(centers,2)
-    y       = zeros(N_basis)
-    iSIGMA  = sigma.^-2
-
-    if velocity > 0
-        y = [sign(centers[velocity,i]) == sign(p[velocity]) ? mnorm_pdf(p,centers[:,i],iSIGMA) : 0 for i = 1:N_basis]
-    else
-        y = [mnorm_pdf(p,centers[:,i],iSIGMA) for i = 1:N_basis]
-    end
-
-    if normalize
-        y ./= (sum(y)+0.000001)
-    end
-    return y
-end
-
-
-
-
-# From gym =====================================================================
-
-
-cp = linspace(-1,0,4)
-cv = linspace(-0.3,0.3,4)
-
-grid1 = meshgrid(cp,cv)
-const c1 = [grid1[1][:] grid1[2][:]]
-const P = size(c1,1)
-const gamma = 0.5P./(c1[end,:] - c1[1,:])'
-
-function ϕ(s)
-    a = exp(-sum((gamma.*(s'.-c1)).^2,2))[:]
-    a ./= (sum(a)+1e-6)
-    a
-end
-
-
-
-# Curated ======================================================================
 
 ## Types
-abstract BasisFunctionExpansion{dim}
-(b::BasisFunctionExpansion)(x) = b.activation(x)
+abstract BasisFunctionExpansion{N}
 
-
-immutable UniformRBFE{1} <: BasisFunctionExpansion{1}
-    activation::Function
-    μ::Array{Float64,1}
-    σ::Float64
-
-    """RBFE{1}(μ::AbstractVector, σ::Real, activation=squared_exponential)
-    Supply all parameters
-    """
-    function RBFE{1}(μ::AbstractVector, σ::Real, activation=squared_exponential)
-        new(squared_exponential,μ,σ)
+function Base.show(b::BasisFunctionExpansion)
+    s = string(typeof(b),"\n")
+    for fn in fieldnames(b)
+        s *= string(fn) * ": " * string(getfield(b,fn)) * "\n"
     end
-
-    """RBFE{1}(v::AbstractVector, Nv::Int; normalize=false, coulomb=false)
-    Supply scheduling signal and number of basis functions for automatic selection of centers and widths
-    """
-    function RBFE{1}(v::AbstractVector, Nv::Int; normalize=false, coulomb=false)
-        activation, μ, γ = basis_activation_func_automatic(v,Nv,normalize,coulomb)
-        new(activation,μ,γ2σ(γ))
-    end
+    println(s)
 end
+
+Base.display(b::BasisFunctionExpansion) = show(b)
+
+### UniformRBFE ================================================================
+immutable UniformRBFE <: BasisFunctionExpansion{1}
+    activation::Function
+    μ::Vector{Float64}
+    σ::Float64
+end
+
+"""UniformRBFE(μ::Vector, σ::Float, activation=squared_exponential)
+Supply all parameters. OBS! `σ` can not be an integer, must be some kind of AbstractFloat
+"""
+function UniformRBFE(μ::AbstractVector, σ::AbstractFloat, activation=squared_exponential)
+    UniformRBFE(activation,μ,σ)
+end
+
+"""UniformRBFE(v::Vector, Nv::Int; normalize=false, coulomb=false)
+Supply scheduling signal and number of basis functions For automatic selection of centers and widths
+"""
+function UniformRBFE(v::AbstractVector, Nv::Int; normalize=false, coulomb=false)
+    activation, μ, γ = basis_activation_func_automatic(v,Nv,normalize,coulomb)
+    UniformRBFE(activation,μ,γ2σ(γ))
+end
+
+
+### RBFE =======================================================================
+immutable MultiUniformRBFE{N} <: BasisFunctionExpansion{N}
+    activation::Function
+    μ::Matrix{Float64}
+    Σ::Vector{Float64}
+end
+
+"""MultiUniformRBFE(μ::Matrix, Σ::Matrix, activation=squared_exponential)
+Supply all parameters.
+"""
+function MultiUniformRBFE(μ::AbstractMatrix, Σ::AbstractMatrix, activation=squared_exponential)
+    @assert length(Nv) == size(μ,1)
+    MultiUniformRBFE{length(Nv)}(activation,μ,Σ)
+end
+
+"""MultiUniformRBFE(v::AbstractVector, Nv::Vector{Int}; normalize=false, coulomb=false)
+Supply scheduling signal and number of basis functions For automatic selection of centers and widths
+"""
+function MultiUniformRBFE(v::AbstractMatrix, Nv::AbstractVector{Int}; normalize=false, coulomb=false)
+    @assert !coulomb "Coulomb not yet supported for multi-dimensional BFEs"
+    @assert length(Nv) == size(v,2)
+    activation, μ, γ = basis_activation_func_automatic(v,Nv,normalize,coulomb)
+    MultiUniformRBFE{length(Nv)}(activation,μ,γ2σ(γ))
+end
+
+
+
+
 
 
 ## Squared exponential functions
 
-squared_exponential(v,vc,gamma) = exp(-gamma*(v.-vc).^2)
+squared_exponential(v::Real,vc,gamma) = exp(-gamma*(v.-vc).^2)
+squared_exponential(v::AbstractVector,vc,gamma::Number) = exp(-gamma*(v.-vc').^2)
+squared_exponential(v::AbstractVector,vc,gamma::AbstractVector) = exp(-((vc'.-v').^2)*gamma)
+function squared_exponential(v::AbstractMatrix,vc,gamma::AbstractVector)
+    a = Matrix{Float64}(size(v,1),size(vc,2))
+    for i = 1:size(v,1)
+        a[i,:] = exp(-sum(gamma.*(v[i,:].-vc).^2,1))
+    end
+    a
+end
 
 function normalized_squared_exponential(v,vc,gamma)
     r = squared_exponential(v,vc,gamma)
-    r ./= (sum(r) + 1e-8)
+    r ./= (sum(r,2) + 1e-8)
 end
 
 squared_exponential_coulomb(v,vc,gamma) = squared_exponential(v,vc,gamma).*(sign(v) .== sign(vc))
 
 function normalized_squared_exponential_coulomb(v,vc,gamma)
     r = squared_exponential_coulomb(v,vc,gamma)
-    r ./= (sum(r,1) + 1e-8)
+    r ./= (sum(r,2) + 1e-8)
 end
 
 function squared_exponential(v::AbstractMatrix,vc, sigma, velocity::Int=0)
+    error("This function has not yet been revised")
     @assert size(v,2) == length(sigma)
     N_basis = size(vc,2)
     y       = zeros(N_basis)
@@ -101,7 +108,7 @@ function squared_exponential(v::AbstractMatrix,vc, sigma, velocity::Int=0)
     end
 end
 
-"""basis_activation_func(v,Nv,normalize,coulomb)
+"""basis_activation_func_automatic(v,Nv,normalize,coulomb)
 
 Returns a func v->ϕ(v) ∈ ℜ(Nv) that calculates the activation of `Nv` basis functions spread out to cover v nicely. If coulomb is true, then we get twice the number of basis functions, 2Nv
 """
@@ -134,27 +141,24 @@ function get_centers_automatic(v::AbstractVector,Nv::Int,coulomb = false)
     vc,gamma
 end
 
-function get_centers_automatic(v::AbstractMatrix, Nv::AbstractVector{Int64}, coulomb=false, coulombdims=0)
+
+function get_centers_automatic(v::AbstractMatrix, Nv::AbstractVector{Int}, coulomb=false, coulombdims=0)
     @assert !coulomb "Coulomb not yet supported for multi-dimensional BFEs"
-    dims    = size(v,2)
-    minq    = minimum(v,1)
-    maxq    = maximum(v,1)
-    dims    = length(Nv)
-    Nbasis  = prod(Nv)
-    centers = Array{Float64,3}(dims,dims,Nbasis)
-    for i = 1:dims
-        bounds = [minq[[i i+dims]]' maxq[[i i+dims]]']
-        centers[i,:,:] = get_centers(bounds, Nv)
-    end
-    return centers
+    @assert size(v,2) == length(Nv) "size(v,2) != length(Nv)"
+    dims   = size(v,2)
+    minq   = minimum(v,1)[:]
+    maxq   = maximum(v,1)[:]
+    bounds = [minq maxq]
+    get_centers(bounds, Nv)
 end
+
 
 function get_centers(bounds, Nv, coulomb=false, coulombdims=0)
     # TODO: split centers on velocity dim
     @assert !coulomb "Coulomb not yet supported for multi-dimensional BFEs"
-    dims = length(Nv);
-    interval = [(bounds[n,2]-bounds[n,1])/(Nv)[n] for n = 1:dims];
-    C = [linspace(bounds[n,1]+interval[n]/2,bounds[n,2]-interval[n]/2,(Nv)[n]) for n = 1:dims];
+    dims     = length(Nv)
+    interval = [(bounds[n,2]-bounds[n,1])/Nv[n] for n = 1:dims]
+    C = [linspace(bounds[n,1]+interval[n]/2,bounds[n,2]-interval[n]/2,(Nv)[n]) for n = 1:dims]
 
     Nbasis  = prod(Nv)
     centers = zeros(dims, Nbasis)
@@ -165,7 +169,7 @@ function get_centers(bounds, Nv, coulomb=false, coulombdims=0)
         centers[i,:] = vec(repmat(C[i]',v,h))'
         h *= Nv[i]
     end
-    centers
+    centers, interval
 end
 
 ## Utility functions
@@ -185,17 +189,25 @@ end
 γ2σ(γ) = √(1./(2γ))
 σ2γ(σ) = 1./(2σ.^2)
 
-function meshgrid(a,b)
+function meshgrid(a::AbstractVector,b::AbstractVector)
     grid_a = [i for i in a, j in b]
     grid_b = [j for i in a, j in b]
     grid_a, grid_b
 end
 
-function meshgrid(a,b,c)
+function meshgrid(a::AbstractVector,b::AbstractVector,c::AbstractVector)
     grid_a = [i for i in a, j in b, k in c]
     grid_b = [j for i in a, j in b, k in c]
     grid_c = [k for i in a, j in b, k in c]
     grid_a, grid_b, grid_c
 end
+# This code must be run after all type definitions!
+
+# for T in subtypes(BasisFunctionExpansion)
+#     (b::T)(x) = b.activation(x)
+# end
+
+(b::UniformRBFE)(x) = b.activation(x)
+(b::MultiUniformRBFE)(x) = b.activation(x)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,26 @@ using BasisFunctionExpansions
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+@test BasisFunctionExpansions.get_centers_automatic(1:10,5,false)[1] |> length == 5
+@test BasisFunctionExpansions.get_centers_automatic(1:10,5,true)[1] |> length == 10
+
+@test BasisFunctionExpansions.get_centers_automatic([1:10 2:11],[5,6])[1] |> size == (5*6,2)
+
+
+rbf = UniformRBFE(1:3, 5)
+@test size(rbf(randn(10))) == (10,5)
+@test size(rbf(randn())) == (5,)
+
+x = linspace(0,4,50)
+a = rbf(x)
+@test a[1] == a[end] # Should be completely symmetric in this case
+@test isa(rbf, BasisFunctionExpansion{1})
+@test !isa(rbf, BasisFunctionExpansion{2})
+
+
+
+v   = [1:3 2:4]
+Nv  = [2,3]
+rbf = MultiUniformRBFE(v,Nv)
+@test rbf(randn(2)) |> size == (prod(Nv),)
+@test rbf(randn(10,2)) |> size == (10,prod(Nv))
